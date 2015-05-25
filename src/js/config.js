e8a8b76f50ff91e4d11687c0390ea36d4b1177fa
Pierre JdlF <minutpapillon@gmail.com>
2015-05-25 11:45:04 +0200

    todo meta in es

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,5 @@
 angular.module('config', [])
 
-.constant('settings', {dev:true,disquskey:'OqPLew400064q8tSFhTrqowfNxZC9jR2Lit9A9Pe1Xwej5M83vVu1cILYamM5cbG',datapath:'../data/',assets:'./',lastupdate:'25 May 2015 - 11:43'})
+.constant('settings', {dev:true,disquskey:'OqPLew400064q8tSFhTrqowfNxZC9jR2Lit9A9Pe1Xwej5M83vVu1cILYamM5cbG',datapath:'../data/',assets:'./',lastupdate:'25 May 2015 - 11:44'})
 
 ;
