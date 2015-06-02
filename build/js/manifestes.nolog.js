8bba62b6b09267f3674a202b40dda1f05f8a2a08
Pierre JdlF <minutpapillon@gmail.com>
2015-06-02 12:46:33 +0200

    image credits, es links

--- build/js/manifestes.nolog.js
+++ build/js/manifestes.nolog.js
@@ -2917,7 +2917,7 @@ angular.module('manifest', [
 
 angular.module('config', [])
 
-.constant('settings', {dev:false,disquskey:'OqPLew400064q8tSFhTrqowfNxZC9jR2Lit9A9Pe1Xwej5M83vVu1cILYamM5cbG',datapath:'data/',assets:'build/',lastupdate:'01 June 2015 - 10:07'})
+.constant('settings', {dev:false,disquskey:'OqPLew400064q8tSFhTrqowfNxZC9jR2Lit9A9Pe1Xwej5M83vVu1cILYamM5cbG',datapath:'data/',assets:'build/',lastupdate:'02 June 2015 - 12:46'})
 
 ;
 ;
