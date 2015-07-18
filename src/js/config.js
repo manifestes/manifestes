1bc59df4bb92a1d14f432bd8e68941b4fc4d1a8a
Pierre JdlF <minutpapillon@gmail.com>
2015-07-19 00:49:54 +0200

    screen.css sizes

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,5 @@
 angular.module('config', [])
 
-.constant('settings', {dev:true,disquskey:'OqPLew400064q8tSFhTrqowfNxZC9jR2Lit9A9Pe1Xwej5M83vVu1cILYamM5cbG',datapath:'../data/',assets:'./',lastupdate:'19 July 2015 - 12:42'})
+.constant('settings', {dev:true,disquskey:'OqPLew400064q8tSFhTrqowfNxZC9jR2Lit9A9Pe1Xwej5M83vVu1cILYamM5cbG',datapath:'../data/',assets:'./',lastupdate:'19 July 2015 - 12:49'})
 
 ;
