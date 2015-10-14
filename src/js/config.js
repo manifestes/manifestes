cff1946d61672b1dacf1b50fd2a5b908639558ff
Pierre JdlF <minutpapillon@gmail.com>
2015-10-14 13:01:49 +0200

    better map legend with links

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,5 @@
 angular.module('config', [])
 
-.constant('settings', {dev:true,disquskey:'OqPLew400064q8tSFhTrqowfNxZC9jR2Lit9A9Pe1Xwej5M83vVu1cILYamM5cbG',datapath:'../data/',assets:'./',lastupdate:'14 October 2015 - 12:46'})
+.constant('settings', {dev:true,disquskey:'OqPLew400064q8tSFhTrqowfNxZC9jR2Lit9A9Pe1Xwej5M83vVu1cILYamM5cbG',datapath:'../data/',assets:'./',lastupdate:'14 October 2015 - 1:01'})
 
 ;
