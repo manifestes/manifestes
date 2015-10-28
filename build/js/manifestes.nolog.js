789995f43151650efc0d7e622f9079ec7b15a6bf
Pierre JdlF <minutpapillon@gmail.com>
2015-10-28 16:21:52 +0100

    add tdfda aka 48 points on map + legend

--- build/js/manifestes.nolog.js
+++ build/js/manifestes.nolog.js
@@ -3193,7 +3193,7 @@ angular.module('manifest', [
 
 angular.module('config', [])
 
-.constant('settings', {dev:false,disquskey:'OqPLew400064q8tSFhTrqowfNxZC9jR2Lit9A9Pe1Xwej5M83vVu1cILYamM5cbG',datapath:'data/',assets:'build/',lastupdate:'26 October 2015 - 12:55'})
+.constant('settings', {dev:false,disquskey:'OqPLew400064q8tSFhTrqowfNxZC9jR2Lit9A9Pe1Xwej5M83vVu1cILYamM5cbG',datapath:'data/',assets:'build/',lastupdate:'28 October 2015 - 4:18'})
 
 ;
 ;
--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,5 @@
 angular.module('config', [])
 
-.constant('settings', {dev:true,disquskey:'OqPLew400064q8tSFhTrqowfNxZC9jR2Lit9A9Pe1Xwej5M83vVu1cILYamM5cbG',datapath:'../data/',assets:'./',lastupdate:'26 October 2015 - 12:55'})
+.constant('settings', {dev:true,disquskey:'OqPLew400064q8tSFhTrqowfNxZC9jR2Lit9A9Pe1Xwej5M83vVu1cILYamM5cbG',datapath:'../data/',assets:'./',lastupdate:'28 October 2015 - 4:18'})
 
 ;
