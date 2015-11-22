c5d8c73b1aca84a42ef8a2f14f89d0cb5d68e734
Pierre JdlF <minutpapillon@gmail.com>
2015-11-22 16:00:51 +0100

    fix sections print display

--- build/js/manifestes.js
+++ build/js/manifestes.js
@@ -3194,7 +3194,7 @@ angular.module('manifest', [
 
 angular.module('config', [])
 
-.constant('settings', {dev:false,disquskey:'OqPLew400064q8tSFhTrqowfNxZC9jR2Lit9A9Pe1Xwej5M83vVu1cILYamM5cbG',datapath:'data/',assets:'build/',lastupdate:'22 November 2015 - 3:54'})
+.constant('settings', {dev:false,disquskey:'OqPLew400064q8tSFhTrqowfNxZC9jR2Lit9A9Pe1Xwej5M83vVu1cILYamM5cbG',datapath:'data/',assets:'build/',lastupdate:'22 November 2015 - 4:00'})
 
 ;
 ;
--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,5 @@
 angular.module('config', [])
 
-.constant('settings', {dev:true,disquskey:'OqPLew400064q8tSFhTrqowfNxZC9jR2Lit9A9Pe1Xwej5M83vVu1cILYamM5cbG',datapath:'../data/',assets:'./',lastupdate:'22 November 2015 - 3:55'})
+.constant('settings', {dev:true,disquskey:'OqPLew400064q8tSFhTrqowfNxZC9jR2Lit9A9Pe1Xwej5M83vVu1cILYamM5cbG',datapath:'../data/',assets:'./',lastupdate:'22 November 2015 - 4:00'})
 
 ;
