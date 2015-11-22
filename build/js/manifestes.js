d7a7e9d13042a25a2526695a215d38b56d40f9de
Pierre JdlF <minutpapillon@gmail.com>
2015-11-22 15:56:38 +0100

    hide images on reduced sections

--- build/js/manifestes.js
+++ build/js/manifestes.js
@@ -3194,7 +3194,7 @@ angular.module('manifest', [
 
 angular.module('config', [])
 
-.constant('settings', {dev:false,disquskey:'OqPLew400064q8tSFhTrqowfNxZC9jR2Lit9A9Pe1Xwej5M83vVu1cILYamM5cbG',datapath:'data/',assets:'build/',lastupdate:'22 November 2015 - 3:41'})
+.constant('settings', {dev:false,disquskey:'OqPLew400064q8tSFhTrqowfNxZC9jR2Lit9A9Pe1Xwej5M83vVu1cILYamM5cbG',datapath:'data/',assets:'build/',lastupdate:'22 November 2015 - 3:54'})
 
 ;
 ;
