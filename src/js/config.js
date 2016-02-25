24207c310034b7e7e35eae46e57c0ebe9e1edf86
Annabelle Ameline <minutpapillon@gmail.com>
2016-02-25 23:05:58 +0000

    updated metahome, no more intro swipe to avoid bugs

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,5 @@
 angular.module('config', [])
 
-.constant('settings', {dev:true,datapath:'../data/',assets:'./',lastupdate:'25 February 2016 - 10:56'})
+.constant('settings', {dev:true,datapath:'../data/',assets:'./',lastupdate:'25 February 2016 - 11:05'})
 
 ;
