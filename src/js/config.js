eaa4d391d414fd1efb9b2129d397262369fe1e84
Annabelle Ameline <minutpapillon@gmail.com>
2016-07-11 21:43:19 +0200

    pad(s) links within menu, share button to slugs, better map toggle all buttons

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,5 @@
 angular.module('config', [])
 
-.constant('settings', {dev:true,langs:['fr','es','en'],datapath:'../data/',assets:'./',lastupdate:'11 July 2016 - 9:42'})
+.constant('settings', {dev:true,langs:['fr','es','en'],datapath:'../data/',assets:'./',lastupdate:'11 July 2016 - 9:43'})
 
 ;
