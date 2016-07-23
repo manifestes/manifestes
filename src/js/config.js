c314e156fcbb8c1f4d169642d36d4e709df5d014
Annabelle Ameline <minutpapillon@gmail.com>
2016-07-23 11:25:21 +0200

    new graph 160723

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,5 @@
 angular.module('config', [])
 
-.constant('settings', {dev:true,langs:['fr','es','en'],datapath:'../data/',assets:'./',lastupdate:'20 July 2016 - 12:32'})
+.constant('settings', {dev:true,langs:['fr','es','en'],datapath:'../data/',assets:'./',lastupdate:'23 July 2016 - 11:21'})
 
 ;
