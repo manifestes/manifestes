77a1314bba27fafdf559c98f3cc7ea1bef907141
Annabelle Ameline <minutpapillon@gmail.com>
2016-09-10 17:25:13 +0200

    trying a bigger-icons-tag-sidepanel

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,5 @@
 angular.module('config', [])
 
-.constant('settings', {dev:true,langs:['fr','es','en'],datapath:'../data/',assets:'./',lastupdate:'12 August 2016 - 9:08'})
+.constant('settings', {dev:true,langs:['fr','es','en'],datapath:'../data/',assets:'./',lastupdate:'10 September 2016 - 5:24'})
 
 ;
