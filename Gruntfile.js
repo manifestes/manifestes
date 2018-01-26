ff972b1c8ab7a28ddaf14a5144318adbeab8e7f9
Annabelle Ameline <minutpapillon@gmail.com>
2018-01-26 13:39:24 +0000

    better splash, wip wip wip

--- Gruntfile.js
+++ Gruntfile.js
@@ -141,7 +141,7 @@ module.exports = function(grunt) {
       js: {
         src: [
           'src/vendor/comma-separated-values/csv.min.js',
-          'src/vendor/xml2js/lib/xml2js.js',
+          //'src/vendor/xml2js/lib/xml2js.js',
 
           'src/vendor/leaflet/dist/leaflet.js',
           'src/vendor/Leaflet.MakiMarkers/Leaflet.MakiMarkers.js',
--- src/js/settings.js
+++ src/js/settings.js
@@ -1,5 +1,5 @@
 angular.module('settings', [])
 
-.constant('settings', {dev:true,langs:['fr','es','en'],layouts:['home','texts','quotes','links','pixels','books','network','map','mapprint','ninja','catalog','catalogprint'],datapath:'../data/',assets:'./',lastupdate:'20 January 2018 - 11:27'})
+.constant('settings', {dev:true,langs:['fr','es','en'],layouts:['home','texts','quotes','links','pixels','books','network','map','mapprint','ninja','catalog','catalogprint'],datapath:'../data/',assets:'./',lastupdate:'26 January 2018 - 1:38'})
 
 ;
