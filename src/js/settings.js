e95c5cf742039dc4e9c1d6024ad2c5d026afdf14
Annabelle Ameline <minutpapillon@gmail.com>
2018-03-13 23:26:45 +0000

    big tags buttons, cleaned contents

--- src/js/settings.js
+++ src/js/settings.js
@@ -1,5 +1,5 @@
 angular.module('settings', [])
 
-.constant('settings', {dev:true,langs:['fr','es','en'],layouts:['home','abcd','pixels','books','network','map','mapprint','ninja','catalog','catalogprint'],datapath:'../data/',assets:'./',lastupdate:'01 March 2018 - 10:03'})
+.constant('settings', {dev:true,langs:['fr','es','en'],layouts:['home','abcd','pixels','books','network','map','mapprint','ninja','catalog','catalogprint'],datapath:'../data/',assets:'./',lastupdate:'07 March 2018 - 11:44'})
 
 ;
