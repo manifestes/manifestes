07497faac1a572b1ddb9d0c6886090c27ed383b0
Annabelle Ameline <minutpapillon@gmail.com>
2018-03-22 12:15:00 +0000

    fixed network loading image

--- src/js/settings.js
+++ src/js/settings.js
@@ -1,5 +1,5 @@
 angular.module('settings', [])
 
-.constant('settings', {dev:true,langs:['fr','es','en'],layouts:['home','abcd','pixels','network','map','mapprint','ninja','catalogprint'],datapath:'../data/',assets:'./',lastupdate:'22 March 2018 - 12:10'})
+.constant('settings', {dev:true,langs:['fr','es','en'],layouts:['home','abcd','pixels','network','map','mapprint','ninja','catalogprint'],datapath:'../data/',assets:'./',lastupdate:'22 March 2018 - 12:14'})
 
 ;
