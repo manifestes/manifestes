381b2140b533158b5761d1dbf3d94219a2b1285b
Pierre JdlF <minutpapillon@gmail.com>
2018-04-15 10:01:48 +0200

    grunt

--- src/js/settings.js
+++ src/js/settings.js
@@ -1,5 +1,5 @@
 angular.module('settings', [])
 
-.constant('settings', {dev:true,langs:['fr','es','en'],layouts:['home','abcd','pixels','network','map','mapprint','ninja','catalogprint'],datapath:'../data/',assets:'./',lastupdate:'15 April 2018 - 9:57'})
+.constant('settings', {dev:true,langs:['fr','es','en'],layouts:['home','abcd','pixels','network','map','mapprint','ninja','catalogprint'],datapath:'../data/',assets:'./',lastupdate:'15 April 2018 - 10:01'})
 
 ;
